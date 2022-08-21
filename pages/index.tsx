7d55816d08864b8bf84d4a98ac5be3fa317d63e1
Jocelyne Murphy <jocelynemurphy@gmail.com>
2022-08-21 17:02:00 -0300

    feat: add resume to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,20 +17,24 @@ const Home: NextPage = () => {
           {`Welcome! I'm Joss.`}
         </p>
         <p className={styles.description}>
-          This site is a work in progress — learn more about my previous work by visiting my 
-          <a className={styles.code} href="https://jocelynemurphy.ca/" target="blank">old website. </a> 
+          For a current life update, check out
+          <a className={styles.code} href="https://github.com/jocelynemurphy" target="_blank" rel="noreferrer">my github. </a> 
         </p>
         {/* <p className={styles.description}>
           Read about
           <a className={styles.code} href="/whyidowhatido">why I do what I do. </a> 
         </p> */}
+        <p className={styles.description}>
+          If you want to cut to the chase, 
+          <a className={styles.code} href="/assets/jocelyne_murphy_software_resume.pdf" target="_blank">download my resume. </a> 
+        </p>
+        <p className={styles.description}>
+          This site is a work in progress — learn more about my previous work by visiting my 
+          <a className={styles.code} href="https://jocelynemurphy.ca/" target="blank">old website. </a> 
+        </p>
         {/* <p className={styles.description}>
           Because no one does it alone, check out my
           <a className={styles.code} href="/whyidowhatido">wall of shoutouts. </a> 
-        </p> */}
-        {/* <p className={styles.description}>
-          If you want to cut to the chase, 
-          <a className={styles.code} href="/whyidowhatido">download my resume. </a> 
         </p> */}
         <p className={styles.description}>
           Have a nice day!  &#x263B; 
