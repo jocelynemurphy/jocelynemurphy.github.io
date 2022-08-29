f089b7e655865a4877ebc62b85e0610c32024af7
Jocelyne Murphy <jocelynemurphy@gmail.com>
2022-08-28 22:36:00 -0400

    feat: why I do what I do

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,23 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
-import { extendTheme } from '@chakra-ui/react';
 import styles from '../styles/Home.module.css';
 
 function MyApp({ Component, pageProps } : AppProps) {
+  const [pageLoaded,setPageLoaded] = React.useState(false);
+
+  React.useEffect(()=>{
+    setPageLoaded(true);
+  },[]);
+
   return (
     <>
       <div className={styles.container}>
         <a href="/" className={styles.h2beige}><p> 	&#10032; </p></a>
-        <Component {...pageProps} />
+        { (pageLoaded) ?
+          <Component {...pageProps} />
+          : null
+        }        
         <footer className={styles.footer}>
          &copy; <p><a href="/">  Jocelyne Murphy {new Date().getFullYear()} </a></p>
         </footer>
@@ -18,30 +25,5 @@ function MyApp({ Component, pageProps } : AppProps) {
     </>
   );
 }
-
-// const colors = {
-//   brand: {
-//     900: '#1a365d',
-//     800: '#153e75',
-//     700: '#2a69ac',
-//   },
-// };
-
-// const theme = extendTheme({ colors });
-
-
-// function MyApp({ Component, pageProps }: AppProps) {
-//   return (
-//     <ChakraProvider theme={theme}>
-//       <main className={styles.main}>
-//         <Component {...pageProps} />
-//       </main>
-//       <footer className={styles.footer}>
-//         &copy; <p>  Jocelyne Murphy {new Date().getFullYear()} </p>
-//       </footer>
-//     </ChakraProvider>
-//   );
-  
-// }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import Link from 'next/link';
 import * as React from 'react';
-
 
 const Home: NextPage = () => {
   return (
@@ -20,21 +17,21 @@ const Home: NextPage = () => {
           For a current life update, check out
           <a className={styles.code} href="https://github.com/jocelynemurphy" target="_blank" rel="noreferrer">my github. </a> 
         </p>
-        {/* <p className={styles.description}>
-          Read about
-          <a className={styles.code} href="/whyidowhatido">why I do what I do. </a> 
-        </p> */}
         <p className={styles.description}>
           If you want to cut to the chase, 
           <a className={styles.code} href="./assets/jocelyne_murphy_software_resume.pdf" target="_blank">download my resume. </a> 
         </p>
         <p className={styles.description}>
-          This site is a work in progress — learn more about my previous work by visiting my 
+          To get to know me, read about
+          <a className={styles.code} href="/whyidowhatido">why I do what I do. </a> 
+        </p>
+        <p className={styles.description}>
+          Learn more about my previous work by visiting my 
           <a className={styles.code} href="https://jocelynemurphy.ca/" target="blank">old website. </a> 
         </p>
         {/* <p className={styles.description}>
           Because no one does it alone, check out my
-          <a className={styles.code} href="/whyidowhatido">wall of shoutouts. </a> 
+          <a className={styles.code} href="/whyidowhatido">shoutout wall. </a> 
         </p> */}
         <p className={styles.description}>
           Have a nice day!  &#x263B; 
